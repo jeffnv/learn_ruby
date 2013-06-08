468a000117a347a3f438afa7c57b205226798ffe
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-08 09:02:12 -0700

    removed puzzling if statement, thanks to aaron's keen eyes

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,17 +2,14 @@ VOWELS = ['a','e','i','o','u']
 
 def beginning_cons_count word
   result = 1
-  if
-    word.split('').each_with_index do |char, i|
-      if(VOWELS.include?(char) && (!((char == 'u' && (word[i - 1] == 'q')))))
-        #if the vowel is a 'u' preceded by a q, consider it a consonant, keep searching for a vowel
-        result = i
-        break
-      end
+  word.split('').each_with_index do |char, i|
+    if(VOWELS.include?(char) && (!((char == 'u' && (word[i - 1] == 'q')))))
+      #if the vowel is a 'u' preceded by a q, consider it a consonant, keep searching for a vowel
+      result = i
+      break
     end
   end
-  
-  result
+result
 end
 
 def latinafy(word)
