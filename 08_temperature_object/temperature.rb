2cfe9deab656d5dc7dd328ebac8cff3ceb3c2367
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-09 19:11:21 -0700

    didn't do anything useful

---
+++ 08_temperature_object/temperature.rb
@@ -0,0 +1,18 @@
+def ftoc(input)
+  result = input *  5.0
+  result /= 9.0
+  result -= 17.77777777777
+  result.round(1)
+end
+
+def ctof(input)
+  result = input * 9.0
+  result /= 5.0
+  result += 32
+  result.round(1)
+end
+
+
+class Temperature
+  
+end
