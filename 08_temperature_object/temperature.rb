06c2da275855f221dabe8382e193a2d33bdb82ec
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-11 19:34:45 -0700

    now it passes, next i need to break into sub classes

--- 08_temperature_object/temperature.rb
+++ 08_temperature_object/temperature.rb
@@ -1,14 +1,20 @@
 class Temperature
   
-  attr_reader :f_temp, :c_temp
+  def self.from_celsius temp
+    Temperature.new({:c => temp})
+  end
+  
+  def self.from_fahrenheit temp
+    Temperature.new({:f => temp})
+  end
   
   def initialize(temps = {})
     if(temps[:f] != nil)
       @f_temp = temps[:f]
-      @c_temp = ftoc(@f_temp)
+      @c_temp = Temperature.ftoc(@f_temp)
     elsif(temps[:c]!= nil)
       @c_temp = temps[:c]
-      @f_temp = ctof(@c_temp)
+      @f_temp = Temperature.ctof(@c_temp)
     else
       @c_temp = 0
       @f_temp = 0
@@ -23,14 +29,14 @@ class Temperature
     @f_temp
   end
   
-  def ftoc(input)
+  def self.ftoc(input)
     result = input *  5.0
     result /= 9.0
     result -= 17.77777777777
     result.round(1)
   end
 
-  def ctof(input)
+  def self.ctof(input)
     result = input * 9.0
     result /= 5.0
     result += 32
