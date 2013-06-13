918bfd944f60a4f28a72961654b644e9d9729100
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-12 17:18:33 -0700

    finished the easy peasy timer program

---
+++ 10_timer/timer.rb
@@ -0,0 +1,27 @@
+class Timer
+  attr_reader :seconds
+  def initialize(time = 0)
+    @seconds = time
+  end
+  
+  def seconds=(val = 0)
+    @seconds = val
+  end
+  
+  def time_string
+    hours = @seconds/3600
+    minutes = (@seconds - hours * 3600) / 60
+    seconds = @seconds - (hours * 3600) - (minutes * 60)
+    
+    "#{pad(hours,2)}:#{pad(minutes,2)}:#{pad(seconds,2)}"
+  end
+  
+  #although if hours are greater...
+  def pad(val, number_of_places)
+    str = val.to_s
+    while(str.length < number_of_places) do
+      str = '0' + str
+    end
+    str
+  end
+end
