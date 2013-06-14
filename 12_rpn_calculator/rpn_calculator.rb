9e6b52f65da2e1d8c94458dc4e70419ba67adc51
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-14 05:46:19 -0700

    partially solved using yield, dang that is neat

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -1,7 +1,43 @@
 class RPNCalculator
-  def initialize
-    @value = 0
+  def value
+    @stack[-1]
   end
   
-  attr_reader :value
+  def initialize
+    @stack = []
+  end
+  
+  def push(value)
+    @stack<< value
+  end
+  
+  def math
+    opp2 = pop
+    opp1 = pop
+    push(yield(opp1, opp2))
+  end
+  
+  def plus
+    math{|x,y| x + y}
+  end
+  
+  def minus
+    math{|x,y| x - y}
+  end
+  
+  def times
+    math{|x,y| x * y}
+  end
+  
+  def divide
+    math{|x,y| x / y}
+  end
+  
+  def pop
+    if(@stack.count > 0)
+      @stack.pop
+    else
+      0
+    end
+  end
 end
