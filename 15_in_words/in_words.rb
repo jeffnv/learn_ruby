0320f8f98078cd2f2a7dd0d8457fa22454be6ec8
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-17 10:38:57 -0700

    passes all tests, wouldn't mind refactoring to make it prettier

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -1,64 +1,55 @@
 class Fixnum
   
-def wordify (number)
-  singles     = ['', 'one', 'two', 'three', 'four','five','six','seven','eight','nine']
-  tens        = %w(zeroen ten twenty thirty fourty fifty sixty seventy eighty ninety)
-  teens       = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
-  number      = number.magnitude % 1000 # keep it legal
-
-  puts "over 100 = #{number / 100}"
-  first_part  = ""
-  first_part  = (singles[number / 100] + " hundred") if number >= 100
-
-
-  last_part   = ""
-  number      = number % 100 # get it to the last two digits
-  case number
-  when 0
-    last_part = ""
-  when 1..9
-    last_part = singles[number]
-  when 10..19
-    last_part = teens[number % 10]
-  else
-    last_part = tens[number / 10] + " " + singles[number%10]
+  def in_words
+    if(self.is_a? Integer)
+      mag       = ['', 'thousand', 'million', 'billion', 'trillion']
+      val       = self
+      loopCount = 0
+      result    = []
+      
+      return "zero" if val == 0
+      
+      while val > 0
+        wrds    = wordify(val % 1000)
+        
+        if(! wrds.empty?)
+          result.insert(0, wrds)
+          result.insert(1, mag[loopCount]) if loopCount > 0
+        end
+        
+        val /= 1000
+        loopCount += 1
+      end
+    end
+    result.join(' ').strip
   end
   
-  (first_part + ' ' + last_part).strip
-end
+  private
+  def wordify (number)
+    singles     = ['', 'one', 'two', 'three', 'four','five','six','seven','eight','nine']
+    tens        = %w(zeroen ten twenty thirty forty fifty sixty seventy eighty ninety)
+    teens       = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+    number      = number.magnitude % 1000 # keep it legal
   
-  # def in_words
-  #   if(self.is_a? Integer)
-  #     singles = ['', 'one', 'two', 'three', 'four','five','six','seven','eight','nine']
-  #     tens = %w(ten twenty thirty fourty fifty sixty seventy eighty ninety)
-  #     mag = ['', 'hundred', 'thousand', 'million', 'billion', 'trillion']
-  #     teens = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
-  #     val = self
-  #     count = self.to_s.length
-  #     result = ""
-  #     
-  #     
-  #     
-  #     count.times do |index|
-  #       magnitude = (count - index - 1)
-  #       num
-  #       
-  #       
-  #       
-  #       val = val % (10 ** magnitude)
-  #       if(magnitude <= 1)#less than 100
-  #         case 
-  #           result =  teens[val % 10]
-  # 
-  #         if(self < 10 && self >= 0)
-  #           singles[self]
-  #         end
-  #         val
-  #       else 
-  #       end
-  # 
-  #     end
-  #   end
-  #   result
-  # end
+    first_part  = ""
+    first_part  = (singles[number / 100] + " hundred") if number >= 100
+
+
+    last_part   = ""
+    number      = number % 100 # get it to the last two digits
+    case number
+    when 0
+      last_part = ""
+    when 1..9
+      last_part = singles[number]
+    when 10..19
+      last_part = teens[number % 10]
+    else
+      last_part = tens[number / 10] + " " + singles[number%10]
+    end
+  
+    (first_part + ' ' + last_part).strip
+  end
+  
+
 end
