baaf4f82e8b4a8a908c5593bbf2762924b2b6929
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-23 08:00:46 -0700

    does the first couple of tests, still need to indent

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,7 +1,23 @@
 class XmlDocument
   def initialize
   end
-  def hello
-    <hello/>
+  
+  def method_missing(method, *args, &block)
+    attrs = args[0] || {}
+    result = "<#{method}"
+    
+    attrs.each do |k,v|
+      result << " #{k}='#{v}'"
+    end
+    
+    
+    
+    if(block != nil)
+      result << ">#{yield}</#{method}>"
+    else
+      result << "/>"
+    end
+    
+    result
   end
 end
