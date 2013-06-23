db48fa33febd9ed24639e1ee9b0686a32664e7d9
Jeffrey Fiddler <jeffnv@gmail.com>
2013-06-23 08:31:17 -0700

    did the XML doc problem, but mostly just copied the solution, what a tedious pain.

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,23 +1,33 @@
 class XmlDocument
-  def initialize
+  def initialize(indent = false)
+    @indent = indent
+    @level = 0
   end
   
   def method_missing(method, *args, &block)
     attrs = args[0] || {}
-    result = "<#{method}"
+    result = @indent ? "  " * @level : ""
+    result << "<#{method}"
     
     attrs.each do |k,v|
       result << " #{k}='#{v}'"
     end
     
-    
-    
     if(block != nil)
-      result << ">#{yield}</#{method}>"
+      result << ">"
+      result << "\n" if @indent
+      @level += 1
+      result << "#{yield}"
+      @level -= 1
+      result << "#{"  " * @level}" if @indent
+      
+      result << "</#{method}>"
+      
     else
       result << "/>"
+      
     end
-    
+    result << "\n" if @indent
     result
   end
 end
